0d1f6b7f6c3c996de4d82288f9256106c75141eb
Roushan <sroushan2104@gmail.com>
2026-08-28 17:38:27 +0530

    chore: rollback WebGL image sequence experiment

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShaderSequence from '@/components/ShaderSequence';
-
-export default function TestPage() {
-  return (
-    <main className="bg-black min-h-screen">
-      <ShaderSequence />
-    </main>
-  );
-}
